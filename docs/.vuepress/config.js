64be2bde2e900c33cd5354944d311bbf49287211
Alejandro <wds@disnot.com>
2021-03-10 10:12:28 +0800

    add ci

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  title: 'realwds\'blog',
+  description: 'Ghosts default theme for Vuepress',
+  base: '/',
+  theme: 'casper',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false,
+      permalinkBefore: false
+    }
+  },
+  themeConfig: {
+    cover: '/images/cover.jpg',
+    logo: '/images/logo.png',
+    nav: [{
+      text: '首页',
+      link: '/'
+    }, {
+      text: '随笔',
+      link: '/posts'
+    }, {
+      text: '媒体',
+      link: '/media'
+    }, {
+      text: '关于',
+      link: '/about'
+    }],
+
+    footer: [{
+      text: 'Latest Posts',
+      link: '/posts'
+    }, {
+      text: 'Facebook',
+      link: 'https://facebook.com/'
+    }, {
+      text: 'Twitter',
+      link: 'https://twitter.com'
+    }, {
+      text: 'Github',
+      link: 'https://github.com/'
+    }],
+    social: {
+      github: 'https://github.com',
+      twitter: 'https://twitter.com'
+    }
+  }
+}
---
+++ docs/about.md
@@ -0,0 +1,12 @@
+---
+title: About
+type: page
+---
+
+精致的五官是心动的开始，
+
+迷人的气质是动情的深渊
+
+Delicate facial features are the beginning of the heart, 
+
+charming temperament is the abyss of emotion.
